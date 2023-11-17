18de11065278675d34c250a08c7c5dd7ae8bf889
Yun Zhang <zhangyun024@gmail.com>
2023-11-17 12:59:27 -0800

    phase3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage/SignupFormPage';
+import Navigation from './components/Navigation/Navigation';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -17,6 +19,7 @@ function Layout() {
 
   return (
     <>
+      <Navigation isLoaded={isLoaded} />
       {isLoaded && <Outlet />}
     </>
   );
@@ -31,11 +34,11 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>
       },
       {
-        path: '/login',
+        path: "login",
         element: <LoginFormPage />
       },
       {
-        path: "/signup",
+        path: "signup",
         element: <SignupFormPage />
       }
     ]
